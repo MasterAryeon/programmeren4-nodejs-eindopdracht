250d82a219134ee5a97f7e26996541d422c57846
Aron Cornet <aron@h-cornet.nl>
2018-05-11 15:07:14 +0200

    Added initial file structure to server.js. Added config file. This resolves #2 issue.

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 /*
     server.js   -   Main server file
  */
+
+// Initiating all the neccessary variables
+const express = require('express');
+const bodyparser = require('body-parser');
+const morgan = require('morgan');
+
+// Include all configs
+const config = require("./config/config");
+
+// Deeper initiation
+const app = express();
+const port = process.env.PORT || config.port;
+
+// Tell the express system to use morgan and bodyparser
+app.use(morgan('dev'));
+app.use(bodyparser.json());
+
+// Start the server on given port
+app.listen(port, () => {
+    console.log('Server is running on port: ' + port);
+});
