aea40342a94c24efc232dea274e19bcb1762068f
Rick Hamers <rawhamer@student.avans.nl>
2018-05-11 17:05:18 +0200

    Initial router and error handler functionality

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const morgan = require('morgan');
 // Include all configs
 const config = require("./config/config");
 
+//Importing the other classes
+const PLACEHOLDER_routes = require('./routes/PLACEHOLDER_routes');
+
 // Deeper initiation
 const app = express();
 const port = process.env.PORT || config.port;
@@ -17,6 +20,44 @@ const port = process.env.PORT || config.port;
 // Tell the express system to use morgan and bodyparser
 app.use(morgan('dev'));
 app.use(bodyparser.json());
+
+//Parsing the request
+app.use('*', (req, res, next) => {
+    let httpMethod = req.method; //Type of request
+    let requestUrl = req.baseUrl; //URL of request
+    console.log('A ' + httpMethod + ' has been made at ' + requestUrl);
+
+    next();
+});
+
+app.use('/api', PLACEHOLDER_routes);
+
+//Error handling - No endpoint found
+app.use('*', (req, res, next) => {
+    let httpMethod = req.method; //Type of request
+    let requestUrl = req.baseUrl; //URL of request
+    console.log('A ' + httpMethod + ' has been made at ' + requestUrl);
+
+    //Sending error info to the Final error Handler
+    next('ERROR 404: Endpoint does not exist');
+    });
+
+//Final error Handler for for Next(Info)
+app.use((err, req, res, next) => {
+   console.log('FINAL ERROR HANDLER: An error occurred');
+   console.log(err.toString());
+   let requestUrl = req.baseUrl;
+
+   //Creating a response for errors
+
+    const error = {
+        error: error.toString(),
+        url: requestUrl
+    };
+
+    //Responding to the error
+    res.status(500).json(error).end(); //STATUS CODE 500 GELDT NIET ALTIJD!
+});
 
 // Start the server on given port
 app.listen(port, () => {
