753072d63d447c85cfb01966bcf0e9425e94b24d
Aron Cornet <aron@h-cornet.nl>
2018-05-14 20:54:20 +0200

    Added getMaaltijdById functionality via GET requests.

--- controllers/maaltijd_controller.js
+++ controllers/maaltijd_controller.js
@@ -14,13 +14,94 @@ module.exports = {
     getMaaltijdList(req, res, next){
         console.log('-------------------A GET request was made-------------------');
         console.log('--------------------Get all maaltijden----------------------');
-        res.status(200).json({}).end(); //Response to the GET request
+        try {
+            const huisId = req.params.id || -1;
+            assert(huisId >= 0, 'Een of meer properties in de request body ontbreken of zijn foutief');
+
+            const connection = new sql.ConnectionPool(config.sql);
+            connection.connect().then(conn => {
+
+                const statement = new sql.PreparedStatement(conn);
+                statement.input('huisID',sql.Int);
+                statement.prepare('EXEC getMaaltijdenByHouseId @huisID;').then(s => {
+                    s.execute({
+                        huisID: huisId
+                    }).then(result => {
+                        s.unprepare();
+
+                        if(result.recordset.length !== 0) {
+                            res.status(200).json(result.recordset).end();
+                        } else {
+                            next(new ApiError(404, 'Niet gevonden (huisId bestaat niet)'));
+                        }
+
+                    }).catch( err => {
+                        console.log(chalk.red('[MSSQL]    ' + err.message));
+                        next(new ApiError(500, 'Er heeft een interne fout opgetreden. Probeer het later opnieuw'));
+                    });
+                }).catch(err => {
+                    console.log(chalk.red('[MSSQL]    ' + err.message));
+                    next(new ApiError(500, 'Er heeft een interne fout opgetreden. Probeer het later opnieuw'));
+                });
+            }).catch(err => {
+                console.log(chalk.red('[MSSQL]    ' + err.message));
+                next(new ApiError(500, 'Er is op dit moment geen verbinding met de database. Probeer het later opnieuw'));
+            });
+        } catch(error) {
+            next(new ApiError(412, error.message));
+        }
     },
 
     getMaaltijdById(req, res, next) {
         console.log('------------------A GET request was made-------------------');
         console.log('---------------------Get maaltijd by ID--------------------');
-        res.status(200).json({}).end(); //Response to the GET request
+
+        try {
+            const huisId = req.params.id || -1;
+            const maaltijdId = req.params.maaltijdId || -1;
+            assert(huisId >= 0, 'Een of meer properties in de request body ontbreken of zijn foutief');
+            assert(maaltijdId >= 0, 'Een of meer properties in de request body ontbreken of zijn foutief');
+
+            const connection = new sql.ConnectionPool(config.sql);
+            connection.connect().then(conn => {
+
+                const statement = new sql.PreparedStatement(conn);
+                statement.input('huisID',sql.Int);
+                statement.input('maaltijdID',sql.Int);
+                statement.prepare('EXEC getMaaltijdWithIdFromHouseId @huisID, @maaltijdID;').then(s => {
+                    s.execute({
+                        huisID: huisId,
+                        maaltijdID: maaltijdId
+                    }).then(result => {
+                        s.unprepare();
+
+                        const records = result.recordset;
+                        if(records.length !== 0) {
+                            if(records[0].result !== -1) {
+                                res.status(200).json(result.recordset[0]).end();
+                            } else {
+                                console.log(chalk.red('[MSSQL]    ' + 'Niet gevonden (huisId bestaat niet)'));
+                                next(new ApiError(404, 'Niet gevonden (huisId bestaat niet)'));
+                            }
+                        } else {
+                            console.log(chalk.red('[MSSQL]    ' + 'Niet gevonden (maaltijdId bestaat niet)'));
+                            next(new ApiError(404, 'Niet gevonden (maaltijdId bestaat niet)'));
+                        }
+                    }).catch( err => {
+                        console.log(chalk.red('[MSSQL]    ' + err.message));
+                        next(new ApiError(500, 'Er heeft een interne fout opgetreden. Probeer het later opnieuw'));
+                    });
+                }).catch(err => {
+                    console.log(chalk.red('[MSSQL]    ' + err.message));
+                    next(new ApiError(500, 'Er heeft een interne fout opgetreden. Probeer het later opnieuw'));
+                });
+            }).catch(err => {
+                console.log(chalk.red('[MSSQL]    ' + err.message));
+                next(new ApiError(500, 'Er is op dit moment geen verbinding met de database. Probeer het later opnieuw'));
+            });
+        } catch(error) {
+            next(new ApiError(412, error.message));
+        }
     },
     createMaaltijd (req, res, next) {
         console.log('----------------------A POST request was made---------------------');
--- routes/maaltijd_routes.js
+++ routes/maaltijd_routes.js
@@ -10,19 +10,19 @@ const maaltijd_controller = require('../controllers/maaltijd_controller');
 let routes = express.Router();
 
 //The GET maaltijd request
-routes.get('/', maaltijd_controller.getMaaltijdList);
+routes.get('/:id/maaltijd', maaltijd_controller.getMaaltijdList);
 
 //The POST studentenhuis request
-routes.post('/', maaltijd_controller.createMaaltijd);
+routes.post('/:id/maaltijd', maaltijd_controller.createMaaltijd);
 
 //The GET maaltijd by Id request
-routes.get('/:maaltijdId', maaltijd_controller.getMaaltijdById);
+routes.get('/:id/maaltijd/:maaltijdId', maaltijd_controller.getMaaltijdById);
 
 //The PUT maaltijd by Id request
-routes.put('/:maaltijdId', maaltijd_controller.putMaaltijdById);
+routes.put('/:id/maaltijd/:maaltijdId', maaltijd_controller.putMaaltijdById);
 
 //The DELETE maaltijd by Id request
-routes.delete('/:maaltijdId', maaltijd_controller.deleteMaaltijdById);
+routes.delete('/:id/maaltijd/:maaltijdId', maaltijd_controller.deleteMaaltijdById);
 
 module.exports = routes;
 
