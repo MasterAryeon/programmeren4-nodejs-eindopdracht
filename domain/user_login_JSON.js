e12b1ed18c4218a358e69b06554c195913e452f2
Aron Cornet <aron@h-cornet.nl>
2018-05-15 19:31:34 +0200

    Added email validation and firstname/lastname validation to user login/register. Finished authentication tests.

--- domain/user_login_JSON.js
+++ domain/user_login_JSON.js
@@ -14,6 +14,9 @@ class user_login_JSON {
         assert(typeof(email) === 'string', 'Een of meer properties in de request body ontbreken of zijn foutief');
         assert(typeof(password) === 'string', 'Een of meer properties in de request body ontbreken of zijn foutief');
 
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        assert(regex.test(String(email).toLowerCase()), 'Een of meer properties in de request body ontbreken of zijn foutief');
+
         this.email = email;
         this.password = password;
     }
--- domain/user_register_JSON.js
+++ domain/user_register_JSON.js
@@ -19,6 +19,12 @@ class user_register_JSON {
         assert(typeof(firstname) === 'string', 'Een of meer properties in de request body ontbreken of zijn foutief');
         assert(typeof(lastname) === 'string', 'Een of meer properties in de request body ontbreken of zijn foutief');
 
+        assert(firstname.length > 2, 'Een of meer properties in de request body ontbreken of zijn foutief');
+        assert(lastname.length > 2, 'Een of meer properties in de request body ontbreken of zijn foutief');
+
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        assert(regex.test(String(email).toLowerCase()), 'Een of meer properties in de request body ontbreken of zijn foutief');
+
         this.firstname = firstname;
         this.lastname = lastname;
         this.email = email;
