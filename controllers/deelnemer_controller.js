97f2b80bf30394d8dd21a0f7247e7e6ef07c7247
Aron Cornet <aron@h-cornet.nl>
2018-05-15 16:31:38 +0200

    Added deleteDeelnemer functionality via DELETE requests.

--- controllers/deelnemer_controller.js
+++ controllers/deelnemer_controller.js
@@ -89,7 +89,7 @@ module.exports = {
                         maaltijdID: maaltijdId
                     }).then(result => {
                         s.unprepare();
-                        console.log(result);
+
                         if ('result' in result.recordset[0]) {
                             switch(result.recordset[0].result) {
                                 case 0:
@@ -126,9 +126,63 @@ module.exports = {
     deleteDeelnemer(request, response, next){
         console.log('---------------A DELETE request was made---------------');
         console.log('-------------------DELETE deelnemer--------------------');
-        response.status(200).json({
-            status: 200,
-            message: placeHolder
-        }).end();
+        try {
+            console.log(request.params);
+            const huisId = request.params.id || -1;
+            const maaltijdId = request.params.maaltijdId || -1;
+
+            assert(huisId >= 0, 'Een of meer properties in de request body ontbreken of zijn foutief');
+            assert(maaltijdId >= 0, 'Een of meer properties in de request body ontbreken of zijn foutief');
+
+            const connection = new sql.ConnectionPool(config.sql);
+            connection.connect().then(conn => {
+
+                const statement = new sql.PreparedStatement(conn);
+                statement.input('huisID',sql.Int);
+                statement.input('accountID',sql.Int);
+                statement.input('maaltijdID',sql.Int);
+                statement.prepare('EXEC deleteDeelnemer @huisID, @accountID, @maaltijdID;').then(s => {
+                    s.execute({
+                        huisID: huisId,
+                        accountID: request.header.tokenid,
+                        maaltijdID: maaltijdId
+                    }).then(result => {
+                        s.unprepare();
+
+                        if(result.recordset.length !== 0) {
+                            if ('result' in result.recordset[0]) {
+                                switch(result.recordset[0].result) {
+                                    case 0:
+                                        next(new ApiError(409, 'Conflict (Gebruiker is niet aangemeld)'));
+                                        break;
+                                    case -1:
+                                        next(new ApiError(404, 'Niet gevonden (maaltijdId bestaat niet)'));
+                                        break;
+                                    case -2:
+                                        next(new ApiError(404, 'Niet gevonden (huisId bestaat niet)'));
+                                        break;
+                                }
+                            } else {
+                                response.status(200).json({}).end();
+                            }
+                        } else {
+                            response.status(200).json({}).end();
+                        }
+                    }).catch( err => {
+                        console.log(chalk.red('[MSSQL]    ' + err.message));
+                        next(new ApiError(500, 'Er heeft een interne fout opgetreden. Probeer het later opnieuw'));
+                    });
+                }).catch(err => {
+                    console.log(chalk.red('[MSSQL]    ' + err.message));
+                    next(new ApiError(500, 'Er heeft een interne fout opgetreden. Probeer het later opnieuw'));
+                });
+            }).catch(err => {
+                console.log(chalk.red('[MSSQL]    ' + err.message));
+                next(new ApiError(500, 'Er is op dit moment geen verbinding met de database. Probeer het later opnieuw'));
+            });
+
+        } catch(error) {
+            next(new ApiError(412, error.message));
+        }
     }
 };
