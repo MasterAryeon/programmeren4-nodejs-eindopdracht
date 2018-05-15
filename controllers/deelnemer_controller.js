067bcd52d1393562b2e888c9c66cfbc2a251ac82
Aron Cornet <aron@h-cornet.nl>
2018-05-15 12:53:33 +0200

    Added getDeelnemers functionality via GET requests. Added the deelnemers routes to the server.

--- controllers/deelnemer_controller.js
+++ controllers/deelnemer_controller.js
@@ -3,21 +3,65 @@
  */
 
 const sql = require('mssql');
+const assert = require('assert');
 const config = require('../config/config');
 const ApiError = require('../domain/ApiError');
 const chalk = require('chalk');
-
-
 
 module.exports = {
 
     getDeelnemerList(request, response, next){
         console.log('---------------A GET request was made---------------');
         console.log('-------------------GET deelnemer--------------------');
-        response.status(200).json({
-            status: 200,
-            message: placeHolder
-        }).end();
+        try {
+            const huisId = request.params.id || -1;
+            const maaltijdId = request.params.maaltijdId || -1;
+
+            assert(huisId >= 0, 'Een of meer properties in de request body ontbreken of zijn foutief');
+            assert(maaltijdId >= 0, 'Een of meer properties in de request body ontbreken of zijn foutief');
+
+            const connection = new sql.ConnectionPool(config.sql);
+            connection.connect().then(conn => {
+
+                const statement = new sql.PreparedStatement(conn);
+                statement.input('huisID',sql.Int);
+                statement.input('maaltijdID',sql.Int);
+                statement.prepare('EXEC getDeelnemersFromMaaltijdId @huisID, @maaltijdID;').then(s => {
+                    s.execute({
+                        huisID: huisId,
+                        maaltijdID: maaltijdId
+                    }).then(result => {
+                        s.unprepare();
+                        console.log(result);
+                        if(result.recordset.length !== 0) {
+                            if ('result' in result.recordset[0]) {
+                                if (result.recordset[0].result === -1) {
+                                    next(new ApiError(404, 'Niet gevonden (huisId bestaat niet)'));
+                                } else {
+                                    next(new ApiError(404, 'Niet gevonden (maaltijdId bestaat niet)'));
+                                }
+                            } else {
+                                response.status(200).json(result.recordset).end();
+                            }
+                        } else {
+                            response.status(200).json([]).end();
+                        }
+                    }).catch( err => {
+                        console.log(chalk.red('[MSSQL]    ' + err.message));
+                        next(new ApiError(500, 'Er heeft een interne fout opgetreden. Probeer het later opnieuw'));
+                    });
+                }).catch(err => {
+                    console.log(chalk.red('[MSSQL]    ' + err.message));
+                    next(new ApiError(500, 'Er heeft een interne fout opgetreden. Probeer het later opnieuw'));
+                });
+            }).catch(err => {
+                console.log(chalk.red('[MSSQL]    ' + err.message));
+                next(new ApiError(500, 'Er is op dit moment geen verbinding met de database. Probeer het later opnieuw'));
+            });
+
+        } catch(error) {
+            next(new ApiError(412, error.message));
+        }
     },
 
     createDeelnemer(request, response, next){
--- routes/deelnemer_routes.js
+++ routes/deelnemer_routes.js
@@ -10,13 +10,13 @@ const deelnemer_controller = require('../controllers/deelnemer_controller');
 let routes = express.Router();
 
 //The GET deelnemer request
-routes.get('/studentenhuis', deelnemer_controller.getDeelnemerList);
+routes.get('/:id/maaltijd/:maaltijdId/deelnemer', deelnemer_controller.getDeelnemerList);
 
 //The POST deelnemer request
-routes.post('/studentenhuis', deelnemer_controller.createDeelnemer);
+routes.post(':id/maaltijd/:maaltijdId/deelnemer', deelnemer_controller.createDeelnemer);
 
 //The DELETE deelnemer request
-routes.delete('studentenhuis:huisId', deelnemer_controller.deleteDeelnemer);
+routes.delete(':id/maaltijd/:maaltijdId/deelnemer', deelnemer_controller.deleteDeelnemer);
 
 
 module.exports = routes;
