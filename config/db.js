e3642231d4c80320a781e4e69d0b74049762f466
Aron Cornet <aron@h-cornet.nl>
2018-05-16 11:11:53 +0200

    Added color coding for errors and notifications.

--- config/db.js
+++ config/db.js
@@ -1,19 +1,20 @@
 const sql = require('mssql');
 const config = require('./config');
+const chalk = require('chalk');
 
 module.exports = new Promise(function(resolve, reject) {
     try {
         const connection = new sql.ConnectionPool(config.sql);
         connection.on('error',() => {});
         connection.connect().then(() => {
-            console.log('Connected');
+            console.log(chalk.green('[MSSQL]    Connected to the MSSQL server'));
             resolve(connection);
         }).catch(err => {
-            console.log('Not Connected');
+            console.log(chalk.red('[MSSQL]    ' + err));
             reject(err)
         });
     } catch(err) {
-        console.log(err)
+        console.log(err);
         reject(err)
     }
 });
