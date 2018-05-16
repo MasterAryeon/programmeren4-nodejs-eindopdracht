8369e76b764a79ce6dbd42322bb7d80e83c2e63a
Rick Hamers <rawhamer@student.avans.nl>
2018-05-16 22:30:53 +0200

    Added comments to all files in the "router" folder

--- routes/error_routes.js
+++ routes/error_routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ErrorController = require('../controllers/error_controller');
 let routes = express.Router();
 
-//Telling the router to use the given endpoint 
+//Telling the router to use the given endpoint
 routes.use(ErrorController.endpointNotFound);
 
 // Exporting the routes so they can be used by the other classes
